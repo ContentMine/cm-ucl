d74f25f3269f3a13fe8bad64f68aeb0a6d4b02de
Peter Murray-Rust <peter.murray.rust@googlemail.com>
2017-03-21 11:44:46 +0000

    writing ESWC paper

--- tutorial1.sh
+++ tutorial1.sh
@@ -4,16 +4,14 @@ cd
 # home
 cd workspace/cm-ucl 
 
-
-
 # make the *.svg.html
-norma --project  tutorial --fileFilter ^.*tables/table(\\d+)/table(_\\d+)?\\.svg  --outputDir tutorial --transform svgtable2html
+norma --project  mytest --fileFilter ^.*tables/table(\\d+)/table(_\\d+)?\\.svg  --outputDir mytest --transform svgtable2html
 # make the *.svg.csv 
-norma --project  tutorial --fileFilter ^.*tables/table(\\d+)/table(_\\d+)?\\.svg  --outputDir tutorial --output table.svg.csv  --transform svgtable2csv
+norma --project  mytest --fileFilter ^.*tables/table(\\d+)/table(_\\d+)?\\.svg  --outputDir mytest --output table.svg.csv  --transform svgtable2csv
 # make the png-svg comparison		
-norma --project tutorial --fileFilter ^.*tables/table\\d+$  --output  ./tableRow.html  --htmlDisplay  ^.*/table.png ^.*/table.svg.html
+norma --project mytest --fileFilter ^.*tables/table\\d+$  --output  ./tableRow.html  --htmlDisplay  ^.*/table.png ^.*/table.svg.html
 # create Ctree-level menu of all tables
-norma --project tutorial --output  tables/tableView.html  --htmlAggregate ^.*tables/table\\d+/tableRow.html
+norma --project mytest --output  tables/tableView.html  --htmlAggregate ^.*tables/table\\d+/tableRow.html
 # create project-level menu of all Ctrees
-norma --project tutorial --output tableViewList.html  --projectMenu .*/tables/tableView.html
+norma --project mytest --output tableViewList.html  --projectMenu .*/tables/tableView.html
 
